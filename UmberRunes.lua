990e041b867a05dc15de3fb6f9bab65c7fe8c72d
Robert Herber <rwherber@gmail.com>
2025-10-11 14:49:30 -0400

    chore: make code better

--- UmberRunes.lua
+++ UmberRunes.lua
@@ -13,7 +13,7 @@ local frame_alpha = 1;
 
 -- Create main frames
 local umber_drag_frame = CreateFrame("Frame", "Backgroundframe", UIParent);
-umber_drag_frame:SetPoint("CENTER", umb_x, umb_y);
+umber_drag_frame:SetPoint("CENTER", UMB_X, UMB_Y);
 umber_drag_frame:SetWidth(1);
 umber_drag_frame:SetHeight(1);
 
@@ -29,6 +29,7 @@ drag_position_text:SetText("");
 
 local frames = {};
 local umber_frame = {};
+
 function umber_frame:create(name, width, height, class, construct, update)
 	local self = {};
 	self.name = name;
@@ -42,13 +43,14 @@ function umber_frame:create(name, width, height, class, construct, update)
 end
 
 local function get_frame_enabled(name)
-	if umb_data["frame_" .. name .. "_enabled"] == nil then
-		umb_data["frame_" .. name .. "_enabled"] = true;
+	if UMB_DATA["frame_" .. name .. "_enabled"] == nil then
+		UMB_DATA["frame_" .. name .. "_enabled"] = true;
 		return true;
 	else
-		return umb_data["frame_" .. name .. "_enabled"];
+		return UMB_DATA["frame_" .. name .. "_enabled"];
 	end
 end
+
 local function get_frame_class_enabled(class)
 	if class == nil or class == "" then
 		return true;
@@ -56,27 +58,24 @@ local function get_frame_class_enabled(class)
 		return select(2, UnitClass('player')) == class;
 	end
 end
+
 local function get_frame_size(name)
-	if umb_data["frame_" .. name .. "_size"] == nil then
-		umb_data["frame_" .. name .. "_size"] = 1;
+	if UMB_DATA["frame_" .. name .. "_size"] == nil then
+		UMB_DATA["frame_" .. name .. "_size"] = 1;
 		return 1;
 	else
-		return umb_data["frame_" .. name .. "_size"];
+		return UMB_DATA["frame_" .. name .. "_size"];
 	end
 end
-local function set_frame_enabled(name, value)
-	if value == true or value == false then
-		umb_data["frame_" .. name .. "_enabled"] = value;
-	end
-end
+
 local function set_frame_size(name, value)
 	if tonumber(value) ~= nil then
-		umb_data["frame_" .. name .. "_size"] = value;
+		UMB_DATA["frame_" .. name .. "_size"] = value;
 	end
 end
 
 local function get_frame(name)
-	for i = 1, table.getn(frames) do
+	for i = 1, #frames do
 		if frames[i].name == name and get_frame_class_enabled(frames[i].class) == true then
 			return i;
 		end
@@ -85,33 +84,33 @@ local function get_frame(name)
 end
 
 local function umber_setup()
-	frame_creation_height = 0;
+	local frame_creation_height = 0;
 
-	if umb_x == nil then umb_x = 0; end
-	if umb_y == nil then umb_y = 0; end
-	if umb_data == nil then umb_data = {}; end
-	if umb_combat == nil then umb_combat = false; end
-	if umb_timers == nil then umb_timers = false; end
+	if UMB_X == nil then UMB_X = 0; end
+	if UMB_Y == nil then UMB_Y = 0; end
+	if UMB_DATA == nil then UMB_DATA = {}; end
+	if UMB_COMBAT == nil then UMB_COMBAT = false; end
+	if UMB_TIMERS == nil then UMB_TIMERS = false; end
 
-	if umb_combat == true then frame_alpha = 1; else frame_alpha = 0; end
+	if UMB_COMBAT == true then frame_alpha = 1; else frame_alpha = 0; end
 
-	for i = 1, table.getn(frames) do
+	for i = 1, #frames do
 		frames[i].frame:SetPoint("TOP", umber_main_frame, "TOP", 0, frame_creation_height);
 		frame_creation_height = frame_creation_height - frames[i].height;
 		frames[i].frame:SetWidth(frames[i].width);
 		frames[i].frame:SetHeight(frames[i].height);
 		frames[i].frame:SetFrameLevel(15);
 
-		if umb_data["frame_" .. frames[i].name .. "_enabled"] == nil then
-			umb_data["frame_" .. frames[i].name .. "_enabled"] = true;
-			umb_data["frame_" .. frames[i].name .. "_size"] = 1;
+		if UMB_DATA["frame_" .. frames[i].name .. "_enabled"] == nil then
+			UMB_DATA["frame_" .. frames[i].name .. "_enabled"] = true;
+			UMB_DATA["frame_" .. frames[i].name .. "_size"] = 1;
 		end
 	end
 end
 
 -- Components
 local function update_alpha()
-	if umb_combat == true then
+	if UMB_COMBAT == true then
 		if UnitAffectingCombat("player") == true or frame_locked == 0 then
 			frame_alpha = frame_alpha + delta_time * 5;
 		else
@@ -125,16 +124,6 @@ local function update_alpha()
 	if frame_alpha > 1 then frame_alpha = 1; end
 
 	umber_main_frame:SetAlpha(frame_alpha);
-end
-
-local function format_big_number(value)
-	if (value > 99999999) then return math.floor(value / 1000000) .. " M"; end
-	if (value > 99999) then return math.floor(value / 1000) .. " K"; end
-	return value;
-end
-local function format_percentage_number(value, maximum)
-	if value == 0 or maximun == 0 then return 0; end
-	return (floor((value / (maximum / 100)) * 10 ^ 0 + 0.5) / 10 ^ 0);
 end
 
 local rune_frames = nil;
@@ -155,9 +144,10 @@ local rune_uv_coord_x = { 0, 0.27, 0 };
 local rune_uv_coord_y = { 0.27, 0.53, 0.27 };
 local rune_uv_coord_z = { 0, 0, 0.53 };
 local rune_uv_coord_w = { 0.27, 0.27, 0.80 };
+
 local function setup_runes()
 	if rune_frames == nil then
-		base_frame = frames[get_frame("runes")];
+		local base_frame = frames[get_frame("runes")];
 
 		rune_frames = {};
 		rune_background_textures = {};
@@ -227,7 +217,14 @@ local function setup_runes()
 		end
 	end
 end
+
 local function update_runes()
+	-- Ensure all arrays are initialized
+	if not rune_texts or not rune_cooldown_textures or not rune_foreground_textures or
+		not rune_background_textures or not rune_complete_textures then
+		return;
+	end
+
 	-- Disable blizzard runes
 	RuneFrame:Hide();
 
@@ -235,41 +232,59 @@ local function update_runes()
 	if rune_current_spec ~= current_spec and current_spec ~= nil then
 		rune_current_spec = current_spec;
 		for i = 1, 6 do
-			rune_foreground_textures[i]:SetTexture("Interface\\PlayerFrame\\ClassOverlayDeathKnightRunes");
-			rune_foreground_textures[i]:SetTexCoord(rune_uv_coord_x[current_spec], rune_uv_coord_y[current_spec],
-				rune_uv_coord_z[current_spec], rune_uv_coord_w[current_spec]);
-			rune_cooldown_textures[i]:SetSwipeTexture("Interface\\PlayerFrame\\DK-" ..
-			rune_english_spec_names[current_spec] .. "-Rune-CDFill");
+			if rune_foreground_textures and rune_foreground_textures[i] then
+				rune_foreground_textures[i]:SetTexture("Interface\\PlayerFrame\\ClassOverlayDeathKnightRunes");
+				rune_foreground_textures[i]:SetTexCoord(rune_uv_coord_x[current_spec], rune_uv_coord_y[current_spec],
+					rune_uv_coord_z[current_spec], rune_uv_coord_w[current_spec]);
+			end
+			if rune_cooldown_textures and rune_cooldown_textures[i] then
+				rune_cooldown_textures[i]:SetSwipeTexture("Interface\\PlayerFrame\\DK-" ..
+					rune_english_spec_names[current_spec] .. "-Rune-CDFill");
+			end
 		end
 	end
 
 
 	for i = 1, 6 do
-		rune_start, rune_duration = GetRuneCooldown(rune_ids[i]);
+		local rune_start, rune_duration = GetRuneCooldown(rune_ids[i]);
 
-		if umb_timers == true then
-			if rune_start == 0 then
-				rune_texts[i]:SetText("");
+		if rune_texts and rune_texts[i] then
+			if UMB_TIMERS == true then
+				if rune_start == 0 then
+					rune_texts[i]:SetText("");
+				else
+					local rune_time = math.floor(rune_duration - (GetTime() - rune_start));
+					if rune_time < 0 then rune_time = 0; end
+					rune_texts[i]:SetText(rune_time);
+				end
 			else
-				rune_time = math.floor(rune_duration - (GetTime() - rune_start));
-				if rune_time < 0 then rune_time = 0; end
-				rune_texts[i]:SetText(rune_time);
+				rune_texts[i]:SetText("");
 			end
-		else
-			rune_texts[i]:SetText("");
 		end
 
 		if rune_start ~= 0 and rune_start ~= nil and rune_duration ~= nil then
-			rune_cooldown_textures[i]:SetCooldown(rune_start, rune_duration);
+			if rune_cooldown_textures and rune_cooldown_textures[i] then
+				rune_cooldown_textures[i]:SetCooldown(rune_start, rune_duration);
+			end
 			rune_anim[i] = 1;
-			rune_foreground_textures[i]:SetAlpha(0);
-			rune_background_textures[i]:SetAlpha(1);
+			if rune_foreground_textures and rune_foreground_textures[i] then
+				rune_foreground_textures[i]:SetAlpha(0);
+			end
+			if rune_background_textures and rune_background_textures[i] then
+				rune_background_textures[i]:SetAlpha(1);
+			end
 		else
 			if rune_anim[i] == 1 then
 				rune_anim[i] = 0;
-				rune_cooldown_textures[i]:SetCooldown(0, 0);
-				rune_foreground_textures[i]:SetAlpha(1);
-				rune_background_textures[i]:SetAlpha(0);
+				if rune_cooldown_textures and rune_cooldown_textures[i] then
+					rune_cooldown_textures[i]:SetCooldown(0, 0);
+				end
+				if rune_foreground_textures and rune_foreground_textures[i] then
+					rune_foreground_textures[i]:SetAlpha(1);
+				end
+				if rune_background_textures and rune_background_textures[i] then
+					rune_background_textures[i]:SetAlpha(0);
+				end
 				rune_glow_anim[i] = 1;
 			end
 		end
@@ -281,12 +296,14 @@ local function update_runes()
 			if rune_glow_anim[i] < 0 then
 				rune_glow_anim[i] = 0;
 			end
-			rune_complete_textures[i]:SetAlpha(math.sin(rune_glow_anim[i] * math.pi));
+			if rune_complete_textures and rune_complete_textures[i] then
+				rune_complete_textures[i]:SetAlpha(math.sin(rune_glow_anim[i] * math.pi));
+			end
 		end
 	end
 end
 
-frames[table.getn(frames) + 1] = umber_frame:create("runes", 120, 24, "DEATHKNIGHT", setup_runes, update_runes);
+frames[#frames + 1] = umber_frame:create("runes", 120, 24, "DEATHKNIGHT", setup_runes, update_runes);
 
 -- On update
 local isdragging = false;
@@ -297,9 +314,10 @@ local y_drag_start = 0;
 local x_dist = 0;
 local y_dist = 0;
 local has_init = false;
+
 umber_main_frame:SetScript("OnUpdate", function(self, elapsed)
 	if has_init == false then
-		for i = 1, table.getn(frames) do
+		for i = 1, #frames do
 			if get_frame_class_enabled(frames[i].class) == true then
 				frames[i].construct();
 			end
@@ -311,29 +329,30 @@ umber_main_frame:SetScript("OnUpdate", function(self, elapsed)
 
 	update_alpha();
 
-	window_width = GetScreenWidth() * UIParent:GetEffectiveScale();
-	window_height = GetScreenHeight() * UIParent:GetEffectiveScale();
+	local window_width = GetScreenWidth() * UIParent:GetEffectiveScale();
+	local window_height = GetScreenHeight() * UIParent:GetEffectiveScale();
 
 	if frame_locked == 0 then
-		for i = 1, table.getn(frames) do frames[i].frame:EnableMouse(true); end
+		for i = 1, #frames do frames[i].frame:EnableMouse(true); end
 
 		if isdragging == false and isscaling == false then
 			frame_selected = -1;
-			for i = 1, table.getn(frames) do
+			for i = 1, #frames do
 				if get_frame_enabled(frames[i].name) == true and get_frame_class_enabled(frames[i].class) == true then
-					if GetMouseFocus() == frames[i].frame then frame_selected = i; end
+					-- Check if mouse is over the frame using a safer method
+					if frames[i].frame:IsMouseOver() then frame_selected = i; end
 				end
 			end
-			if IsMouseButtonDown(1) == true then
+			if IsMouseButtonDown("LeftButton") == true then
 				if frame_selected ~= -1 then
 					x_drag_start, y_drag_start = GetCursorPosition();
 					x_drag_start = (x_drag_start - window_width / 2) / UIParent:GetEffectiveScale();
 					y_drag_start = (y_drag_start - window_height / 2) / UIParent:GetEffectiveScale();
-					x_dist = umb_x - x_drag_start;
-					y_dist = umb_y - y_drag_start;
+					x_dist = UMB_X - x_drag_start;
+					y_dist = UMB_Y - y_drag_start;
 					isdragging = true;
 				end
-			elseif IsMouseButtonDown(2) == true then
+			elseif IsMouseButtonDown("RightButton") == true then
 				if frame_selected ~= -1 then
 					x_drag_start, y_drag_start = GetCursorPosition();
 					isscaling = true;
@@ -341,19 +360,19 @@ umber_main_frame:SetScript("OnUpdate", function(self, elapsed)
 			end
 		end
 
-		if isdragging and IsMouseButtonDown(1) == false then isdragging = false end;
-		if isscaling and IsMouseButtonDown(2) == false then isscaling = false end;
+		if isdragging and IsMouseButtonDown("LeftButton") == false then isdragging = false end;
+		if isscaling and IsMouseButtonDown("RightButton") == false then isscaling = false end;
 
 		if isdragging == true then
-			new_x, new_y = GetCursorPosition();
+			local new_x, new_y = GetCursorPosition();
 			new_x = (new_x - window_width / 2) / UIParent:GetEffectiveScale();
 			new_y = (new_y - window_height / 2) / UIParent:GetEffectiveScale();
-			umb_x = new_x + x_dist;
-			umb_y = new_y + y_dist;
+			UMB_X = new_x + x_dist;
+			UMB_Y = new_y + y_dist;
 		elseif isscaling == true then
-			current_scale = get_frame_size(frames[frame_selected].name);
-			xPos, yPos = GetCursorPosition();
-			distance = (y_drag_start - yPos) / 50;
+			local current_scale = get_frame_size(frames[frame_selected].name);
+			local _, yPos = GetCursorPosition();
+			local distance = (y_drag_start - yPos) / 50;
 			current_scale = current_scale - distance;
 			if current_scale < 0.5 then current_scale = 0.5 end
 			if current_scale > 3 then current_scale = 3 end
@@ -361,26 +380,26 @@ umber_main_frame:SetScript("OnUpdate", function(self, elapsed)
 			x_drag_start, y_drag_start = GetCursorPosition();
 		end
 	else
-		for i = 1, table.getn(frames) do frames[i].frame:EnableMouse(false); end
+		for i = 1, #frames do frames[i].frame:EnableMouse(false); end
 	end
 
-	if umb_x < -GetScreenWidth() / 2 then umb_x = -GetScreenWidth() / 2; end
-	if umb_y < -GetScreenHeight() / 2 then umb_y = -GetScreenHeight() / 2; end
-	if umb_x > GetScreenWidth() / 2 then umb_x = GetScreenWidth() / 2; end
-	if umb_y > GetScreenHeight() / 2 then umb_y = GetScreenHeight() / 2; end
-	umb_x = math.floor(umb_x);
-	umb_y = math.floor(umb_y);
+	if UMB_X < -GetScreenWidth() / 2 then UMB_X = -GetScreenWidth() / 2; end
+	if UMB_Y < -GetScreenHeight() / 2 then UMB_Y = -GetScreenHeight() / 2; end
+	if UMB_X > GetScreenWidth() / 2 then UMB_X = GetScreenWidth() / 2; end
+	if UMB_Y > GetScreenHeight() / 2 then UMB_Y = GetScreenHeight() / 2; end
+	UMB_X = math.floor(UMB_X);
+	UMB_Y = math.floor(UMB_Y);
 
 	if frame_locked == 0 then
-		drag_position_text:SetText(umb_x .. ", " .. umb_y);
+		drag_position_text:SetText(UMB_X .. ", " .. UMB_Y);
 	else
 		drag_position_text:SetText("");
 	end
 
-	largest_width = 1;
-	total_height = 1;
-	frame_current_height = 0;
-	for i = 1, table.getn(frames) do
+	local largest_width = 1;
+	local total_height = 1;
+	local frame_current_height = 0;
+	for i = 1, #frames do
 		if get_frame_enabled(frames[i].name) == true and get_frame_class_enabled(frames[i].class) == true then
 			frames[i].frame:SetScale(get_frame_size(frames[i].name));
 			frames[i].frame:Show();
@@ -400,9 +419,9 @@ umber_main_frame:SetScript("OnUpdate", function(self, elapsed)
 	umber_main_frame:SetPoint("TOP", umber_drag_frame, "TOP", 0, 0);
 
 	umber_drag_frame:ClearAllPoints();
-	umber_drag_frame:SetPoint("CENTER", umb_x, umb_y);
+	umber_drag_frame:SetPoint("CENTER", UMB_X, UMB_Y);
 
-	for i = 1, table.getn(frames) do
+	for i = 1, #frames do
 		if get_frame_enabled(frames[i].name) == true and get_frame_class_enabled(frames[i].class) == true then
 			frames[i].update();
 		end
@@ -417,20 +436,15 @@ umber_main_frame:SetScript("OnEvent", function(self, event, ...)
 		umber_setup();
 		umber_main_frame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED");
 	end
-
-	if event == "COMBAT_LOG_EVENT_UNFILTERED" then
-		local _, type, _, sourceGUID, _, _, _, destGUID, _, _, _ = ...;
-		if type == "SWING_DAMAGE" and sourceGUID == UnitGUID("player") and destGUID == UnitGUID("target") then
-			swingtimer_value = GetTime();
-		end
-	end
 end)
 
 -- Slash commands
 local header_start = "|cFFFFA07AUmberRunes: |cffffffff";
 local command_color = "|cFF00FFFF";
 local text_color = "|cffffffff";
+
 SLASH_UMBER1 = '/umber';
+
 local function handler(msg, editbox)
 	local command, rest = msg:match("^(%S*)%s*(.-)$")
 	if command == "lock" then
@@ -440,15 +454,15 @@ local function handler(msg, editbox)
 			frame_locked = 1; print(header_start .. "Frame locked.")
 		end;
 	elseif command == "reset" then
-		umb_x = 0; umb_y = 0;
-		for i = 1, table.getn(frames) do umb_data["frame_" .. frames[i].name .. "_size"] = 1; end
-		for i = 1, table.getn(frames) do umb_data["frame_" .. frames[i].name .. "_enabled"] = true; end
+		UMB_X = 0; UMB_Y = 0;
+		for i = 1, #frames do UMB_DATA["frame_" .. frames[i].name .. "_size"] = 1; end
+		for i = 1, #frames do UMB_DATA["frame_" .. frames[i].name .. "_enabled"] = true; end
 		print(header_start .. "Frame reset.");
 	elseif command == "setscale" then
-		scale = tonumber(rest);
+		local scale = tonumber(rest);
 		if scale ~= nil then
 			if scale >= 0.5 and scale <= 3 then
-				for i = 1, table.getn(frames) do umb_data["frame_" .. frames[i].name .. "_size"] = scale; end
+				for i = 1, #frames do UMB_DATA["frame_" .. frames[i].name .. "_size"] = scale; end
 			else
 				print(header_start .. "Use a number between 0.5 and 3 to set the scale.");
 			end
@@ -456,18 +470,20 @@ local function handler(msg, editbox)
 			print(header_start .. "Use a number between 0.5 and 3 to set the scale.");
 		end
 	elseif command == "combat" then
-		if umb_combat == true then
-			umb_combat = false; print(header_start .. "Will remain visible while out of combat.")
+		if UMB_COMBAT == true then
+			UMB_COMBAT = false; print(header_start .. "Will remain visible while out of combat.")
 		else
-			umb_combat = true; print(header_start .. "Will hide while out of combat.")
+			UMB_COMBAT = true; print(header_start .. "Will hide while out of combat.")
 		end;
 	else
 		print("|cFFFFA07AUmberRunes:");
 		print(command_color .. "/umber lock - " .. text_color .. "Lock/unlock the main frame.");
 		print(command_color .. "/umber reset - " .. text_color .. "Reset the position of the main frame.");
 		print(command_color ..
-		"/umber setscale - " .. text_color .. "Set the scale of all components. (Value between 0.5 and 3, 1 is default).");
+			"/umber setscale - " ..
+			text_color .. "Set the scale of all components. (Value between 0.5 and 3, 1 is default).");
 		print(command_color .. "/umber combat - " .. text_color .. "Toggle hiding when out of combat.");
 	end
 end
+
 SlashCmdList["UMBER"] = handler;
